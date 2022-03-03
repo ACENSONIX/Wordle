f9d31aa7b687a0b124d068305a4ad9e4baf937ed
Sahil Shetty <70585867+ACENSONIX@users.noreply.github.com>
2022-03-04 00:46:23 +0530

    backspace is working now

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-function goto() {}
 var j = 1;
 var i = 0;
-let word = "SAHIL";
 
 function getdata() {
-  if (i <= 4) {
+  console.log("yes");
+  if (i <= 3) {
     var arr = document.getElementById("line-" + j).children;
 
-    if (i - 1 >= 0) {
-      document.getElementById(arr[i - 1].id).setAttribute("readOnly", "true");
+    if (i >= 0) {
+      document.getElementById(arr[i].id).setAttribute("readOnly", "true");
     }
     document.getElementById(arr[i + 1].id).focus();
     document.getElementById(arr[i + 1].id).removeAttribute("readonly");
-    i++;
+    if (i < 3) i++;
   }
 }
 
 function del_enter() {
   var key = event.keyCode || event.charCode;
-  console.log(key);
-  if (i == 4 && key == 13) {
+  // console.log(key);
+  if (i == 3 && key == 13) {
     validate();
     let next_line = document.getElementById("line-" + j).children;
     document.getElementById(next_line[0].id).focus();
     i = 0;
+  }
+  if (i >= 0 && j < 7 && key == 8) {
+    console.log(i);
+    backspace();
+    console.log(i);
   }
 }
 
@@ -36,12 +40,23 @@ function validate() {
     in_ans = in_ans + document.getElementById(alpha.id).value;
     document.getElementById(alpha.id).readOnly = true;
     if (in_ans[k] == new_word[k]) {
-      document.getElementById(alpha.id).style.backgroundColor =
-        "rgb(223, 207, 62)";
-    } else {
-      document.getElementById(alpha.id).style.backgroundColor =
-        "rgb(149, 145, 158)";
+      document.getElementById(alpha.id).style.backgroundColor = "#538d4e";
+    } else if (new_word.includes(in_ans[k])) {
+      document.getElementById(alpha.id).style.backgroundColor = "#b59f3b";
     }
   }
   j++;
+}
+
+function backspace() {
+  // console.log(i);
+  let bac = document.getElementById("line-" + j).children;
+  let beta = document.getElementById(bac[i + 1].id);
+  document.getElementById(beta.id).value = "";
+  document.getElementById(beta.id).readOnly = true;
+  if (i >= 0) {
+    document.getElementById(bac[i].id).removeAttribute("readonly");
+    document.getElementById(bac[i].id).focus();
+  }
+  if (i != 0) i--;
 }
